82c654201beb5c69d86ef6ecda9ee31dbc85bfd0
Charles Wang <czw@seas.upenn.edu>
2024-02-03 14:37:22 -0500

    working clock

--- FriendAscend/Assets/Scripts/UI/Timer.cs
+++ FriendAscend/Assets/Scripts/UI/Timer.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
-public class Timer : MonoBehaviour
+namespace UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Timer : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private GameObject clockHand;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private bool _paused;
+        private float _time;
+
+        private void Update()
+        {
+            if (_paused)
+            {
+                return;
+            }
+
+            _time += Time.deltaTime;
+            
+            var eulerAngles = clockHand.transform.eulerAngles;
+            clockHand.transform.rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, _time * -60f);
+
+            var ts = new TimeSpan((long) (_time * 10_000_000));
+            text.text = ts.ToString(@"h\:mm\:ss\.fff");
+        }
+
+        public void ResetTimer() => _time -= _time;
+        public void Pause() => _paused = true;
+        public void Continue() => _paused = false;
     }
 }

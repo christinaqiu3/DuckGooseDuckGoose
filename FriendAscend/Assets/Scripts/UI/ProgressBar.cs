1ddda012b3e0bf518eae0cb71373d864418b9d72
Charles Wang <czw@seas.upenn.edu>
2024-02-03 20:30:28 -0500

    done

--- FriendAscend/Assets/Scripts/UI/ProgressBar.cs
+++ FriendAscend/Assets/Scripts/UI/ProgressBar.cs
@@ -6,6 +6,8 @@ namespace UI
     {
         [SerializeField] private Warning warning;
         [SerializeField] private WarningSign warningSign;
+        [SerializeField] private Tick gooseTick;
+        [SerializeField] private Tick duckTick;
         
         [SerializeField, Range(0f, 1f)] private float progGoose;
         [SerializeField, Range(0f, 1f)] private float progDuck;
@@ -29,6 +31,9 @@ namespace UI
             {
                 warning.CalculateHeight(progDuck, progGoose);
             }
+            
+            gooseTick.CalculatePos(progGoose);
+            duckTick.CalculatePos(progDuck);
         }
 
         public void EnableWarning()

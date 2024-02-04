fb7d660163ba6d9cc11ac56f99d89cf97d75959c
virajdoshi02 <47150131+virajdoshi02@users.noreply.github.com>
2024-02-03 21:27:00 -0500

    Jumping FIXED??

--- FriendAscend/Assets/Scripts/PlatformScript.cs
+++ FriendAscend/Assets/Scripts/PlatformScript.cs
@@ -14,21 +14,21 @@ public class PlatformScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<MeshFilter>().mesh = platformMeshes[(int)Random.value * platformMeshes.Length];
+        GetComponent<MeshFilter>().mesh = platformMeshes[(int)(Random.value*platformMeshes.Length)];
 
         if (platformNo == maxNumPlatforms)
             return;
         platformNo = GameObject.FindGameObjectsWithTag("FriendAscendPlatform").Length;
         GameObject nextPlatform = Instantiate(platform);
 
-        nextPlatform.transform.position = gameObject.transform.position + gameObject.transform.localScale / 2
-            +(platformNo==1? Vector3.zero: new Vector3((Random.value-0.3f)*horizontalOffset,(Random.value-1f)*verticalOffset, (Random.value - 0.3f)*horizontalOffset));
+        nextPlatform.transform.position = gameObject.transform.position + gameObject.GetComponent<BoxCollider>().bounds.size / 2
+            +(platformNo==1? Vector3.zero: new Vector3((Random.value-0.3f)*horizontalOffset,(Random.value-0.5f)*verticalOffset, (Random.value - 0.3f)*horizontalOffset));
 
         if(nextPlatform.transform.position[1]-transform.position[1]>3)
         {
             GameObject plat = Instantiate(miniPlatform);
-            plat.transform.position = nextPlatform.transform.position - nextPlatform.transform.localScale / 2
-                + Vector3.up * nextPlatform.transform.localScale.y * 2/3.0f + new Vector3((Random.value - 0.7f) * horizontalOffset/2.0f, (Random.value - 1f) * verticalOffset/4.0f, (Random.value - 0.7f) * horizontalOffset/2.0f);
+            plat.transform.position = nextPlatform.transform.position - nextPlatform.GetComponent<BoxCollider>().bounds.size / 2
+                + Vector3.up * nextPlatform.GetComponent<BoxCollider>().bounds.size.y * 2/3.0f + new Vector3((Random.value - 0.7f) * horizontalOffset/2.0f, (Random.value - 1f) * verticalOffset/3.0f, (Random.value - 0.7f) * horizontalOffset/2.0f);
 
             plat.transform.parent = gameObject.transform.parent;
         }
